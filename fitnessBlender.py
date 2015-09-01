3f267ea164c088c1cd70d3446d5dc2c58e828c38
jdollar <dollarhidejoel@gmail.com>
2015-09-01 01:29:33 -0400

    Added menu item and input for keyword search. No functionality. Still returns all videos. Refactored constants to their own file. Spike for XSD specifications instead. Added gitignore for generated pyo files

--- fitnessBlender.py
+++ fitnessBlender.py
@@ -1,5 +1,7 @@
 import sys
 
+from resources.lib.fitnessblender.constants import Constants
+
 import requests
 from bs4 import BeautifulSoup 
 
@@ -10,15 +12,6 @@ import xbmcgui
 import xbmc
 import xbmcplugin
 
-ALL_VIDEO_MENU_ITEM = 'allVideos'
-
-TYPE_PARAM = 'type'
-LINK_PARAM = 'link'
-PAGE_NUM_PARAM = 'pageNum'
-
-DEFAULT_VIDEO_IMAGE = 'DefaultVideo.jpg'
-
-mainMenuItems = {'Videos':ALL_VIDEO_MENU_ITEM}
 
 basePluginUrl = sys.argv[0][:-1]
 addon_handle = int(sys.argv[1])
@@ -27,17 +20,16 @@ fitnessBlenderBaseUrl = 'https://www.fitnessblender.com'
 dialog = xbmcgui.Dialog()
 
 args = urlparse.parse_qs(sys.argv[2][1:])
-typeParam = args.get(TYPE_PARAM, None)
-link = args.get(LINK_PARAM, None)
-videoPageNum = args.get(PAGE_NUM_PARAM, '1')
+typeParam = args.get(Constants.TYPE_PARAM, None)
+link = args.get(Constants.LINK_PARAM, None)
+keywordParam = args.get(Constants.KEYWORD_PARAM, None)
+videoPageNum = args.get(Constants.PAGE_NUM_PARAM, '1')
 
 def buildMainMenu():
-    for menuItemKey, menuItemValue in mainMenuItems.items():
-	dialog.ok('key', menuItemKey)
-	dialog.ok('value', menuItemValue)
-        url = buildUrl({TYPE_PARAM:menuItemValue})
-        li = xbmcgui.ListItem(menuItemKey, iconImage=DEFAULT_VIDEO_IMAGE)
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+    for menuItemKey, menuItemValue in Constants.mainMenuItems.items():
+        url = buildUrl({Constants.TYPE_PARAM:menuItemValue})
+        li = xbmcgui.ListItem(menuItemKey, iconImage=Constants.DEFAULT_VIDEO_IMAGE)
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
@@ -46,21 +38,28 @@ def buildUrl(params):
 
 def findAllVideoLinksOnPage(params):
     response = requests.request('GET', fitnessBlenderBaseUrl + '/videos?' + urllib.urlencode(params))
+    print(response.text)
     soup = BeautifulSoup(response.text,'html.parser')
     return soup
 
-def generateVideoLinks(videoLinkList):
+def generateVideoLinks(videoLinkList, paramList=None):
     for videoLink in videoLinkList:
         image = videoLink.find('img')
-        url = buildUrl({LINK_PARAM:videoLink['href'][8:]})
+        updatedParams = paramList.copy() if paramList is not None else {}
+        updatedParams.update({Constants.LINK_PARAM:videoLink['href'][8:]})
+        url = buildUrl(updatedParams)
         li = xbmcgui.ListItem(image['alt'], iconImage=fitnessBlenderBaseUrl + image['src'])
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-
-    #Build next page option
-    url = buildUrl({PAGE_NUM_PARAM:str(int(videoPageNum[0])+1)})
-    li = xbmcgui.ListItem('Next Page', iconImage=DEFAULT_VIDEO_IMAGE)
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+   
+    generateNextPageItem(paramList)
     xbmcplugin.endOfDirectory(addon_handle)
+
+def generateNextPageItem(paramList):
+    updatedParams = paramList.copy() if paramList is not None else {}
+    updatedParams.update({Constants.PAGE_NUM_PARAM:str(int(videoPageNum[0])+1), Constants.TYPE_PARAM:typeParam[0]})
+    url = buildUrl(updatedParams)
+    li = xbmcgui.ListItem('Next Page', iconImage=Constants.DEFAULT_VIDEO_IMAGE)
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
 def getVideoId():
     response = requests.request('GET', fitnessBlenderBaseUrl + '/videos/' + link[0])
@@ -73,9 +72,16 @@ def playVideo(videoId):
 
 if typeParam is None:
     buildMainMenu()
-elif typeParam is ALL_VIDEO_MENU_ITEM:
+elif typeParam[0] == Constants.ALL_VIDEO_MENU_ITEM:
     soup = findAllVideoLinksOnPage({'p':videoPageNum[0]})
     videoLinkList = soup.find_all('a',class_='videolink')
     generateVideoLinks(videoLinkList)
+elif typeParam[0] == Constants.SEARCH_VIDEO_MENU_ITEM:
+    keywordInput = dialog.input('Keyword', type=xbmcgui.INPUT_ALPHANUM) if keywordParam is None else keywordParam[0]
+    soup = findAllVideoLinksOnPage({'p':videoPageNum[0],'keyword':keywordInput})
+    videoLinkList = soup.find_all('a',class_='videolink')
+    generateVideoLinks(videoLinkList, {'keyword':keywordInput})
 else:
     playVideo(getVideoId())
+
+
---
+++ resources/lib/fitnessblender/constants.py
@@ -0,0 +1,16 @@
+class Constants:
+    ALL_VIDEO_MENU_ITEM = 'allVideos'
+    SEARCH_VIDEO_MENU_ITEM = 'searchVideos'
+    
+    TYPE_PARAM = 'type'
+    LINK_PARAM = 'link'
+    KEYWORD_PARAM = 'keyword'
+    PAGE_NUM_PARAM = 'pageNum'
+    
+    DEFAULT_VIDEO_IMAGE = 'DefaultVideo.jpg'
+    FITNESS_BLENDER_BASE_URL = 'https://www.fitnessblender.com'
+    
+    mainMenuItems = {
+                        'Search':SEARCH_VIDEO_MENU_ITEM,
+                        'Videos':ALL_VIDEO_MENU_ITEM
+                    }
